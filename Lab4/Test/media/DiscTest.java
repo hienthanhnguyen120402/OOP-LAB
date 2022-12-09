64dbed33af2ac0896025cd02bd1746b1b15eca6b
hienthanhnguyen120402 <nguyenthanhhien12042002@gmail.com>
2022-12-09 20:48:16 +0700

    first commit

---
+++ Lab4/Test/media/DiscTest.java
@@ -0,0 +1,12 @@
+package Test.media;
+
+package test.media.disc;
+
+import test.media.MediaObjects;
+
+public class DiscTest extends MediaObjects {
+    public static void main(String[] args) {
+
+        // Lab3: test toString function
+        System.out.println(dvd1.toString());
+    }
